db919ad1312cf19e23be502317dff1b26d074b15
namtaehyun <taehyunnam8034@gmail.com>
2021-09-23 22:38:57 +0900

    Add post

---
+++ pages/posts/first-post.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+      <Image
+        src="/images/panamera.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
+    </>
+  );
+}
