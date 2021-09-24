48b1a3ef31e1b3eb513c1863510e4b93d7856ac0
namtaehyun <taehyunnam8034@gmail.com>
2021-09-24 18:10:09 +0900

    Update post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,19 +1,19 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import Head from 'next/head';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-      <Image
-        src="/images/panamera.jpg" // Route of the image file
-        height={144} // Desired size with correct aspect ratio
-        width={144} // Desired size with correct aspect ratio
-        alt="Your Name"
-      />
-    </>
+      <Image src="/images/panamera.jpg" height={300} width={400} alt="panamera" />
+    </Layout>
   );
 }
