3baa8a84e38d5276d1f121b83397aaf07a56158d
namtaehyun <taehyunnam8034@gmail.com>
2022-01-09 19:01:47 +0900

    feat(Component): Add Date Component

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,18 +1,25 @@
-import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from '../../components/date';
+import Head from 'next/head';
 
 export default function Post({ postData }) {
   const { title, id, date } = postData;
 
   return (
     <Layout>
+      <Head>
+        <title>{title}</title>
+      </Head>
       {title}
       <br />
       {id}
       <br />
-      {date}
+      {<Date dateString={date}></Date>}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
-  )
+  );
 }
 
 export async function getStaticPaths() {
@@ -20,16 +27,16 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id);
 
   return {
     props: {
-      postData
-    }
-  }
+      postData,
+    },
+  };
 }
